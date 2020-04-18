b853c0ec03705e0c27b503793c1ecd8dd792acb7
PareekVaibhav <51473721+PareekVaibhav@users.noreply.github.com>
2020-04-18 13:15:17 +0530

    Add files via upload

---
+++ findrestaurant.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<title>Template Downloader</title>
+<link rel="Stylesheet" type="text/css" href="ppl.css">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+rel="stylesheet">
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+rel="stylesheet">
+</head>
+<body>
+<div class="container">
+<div class="Restaurant">
+<h1>Templates<h1>
+</div>
+<div class="row">
+<div class="col-md-3 text-center">
+<div class="icon">
+<i class="fa fa-desktop"></i>
+</div>
+<h3>BootImage Gallery</h3>
+
+<br>
+</button> 
+<a class="nav-link" href="bootgallery.php">View Template</a>
+</div>
+<div class="col-md-3 text-center">
+<div class="icon">
+<i class="fa fa-tablet"></i>
+</div>
+<h3>Dating App</h3>
+
+<br>
+</button> 
+<a class="nav-link" href="bootstartup.php" >View Template</a>
+</div>
+
+<div class="col-md-3 text-center">
+<div class="icon">
+<i class="fa fa-line-chart"></i>
+</div>
+<h3>Restaurant app</h3>
+
+<br>
+</button> 
+<a class="nav-link" href="index.php" >View Template</a>
+</div>
+
+
+</body>
+</html>
